7a371c4d7d7af536ae05d16072eff985d6927876
Dryadalis <kika.romanska@gmail.com>
2018-10-28 13:10:50 +0100

    Auto refresh added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const rp = require('request-promise');
+const cheerio = require('cheerio');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const { title, name } = req.query;
+  res.render('index', { title, name });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send(`<h1>ELOOOO</h1>`);
+});
+
+router.get('/test', function(req, res, next) {
+    res.send(`<h1>ELOOOO</h1>`);
 });
 
 module.exports = router;
